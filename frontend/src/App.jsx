fd45b776224be59b0e9f53f6d2edc00529acf7b4
vanshu778 <vthesiya778@gmail.com>
2024-09-29 19:21:04 +0530

    CORS policy error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[jokes,setjokes] = useState([])
 
+  useEffect(() => {
+    axios.get('http://localhost:3000/jokes').then((response) => {
+      setjokes(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  })
   return (
     <>
       <h1>Full Stack</h1>
+      <p>Jokes: {jokes.length}</p>
+      {
+        jokes.map((joke, index) => {
+            <div key = {joke.id}>
+              <h3>{joke.title}</h3>
+              <p>{joke.content}</p>
+            </div>
+        })
+      }
     </>
   )
 }
