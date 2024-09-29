e367baff7962a9a20e9dd06f621d2299467af449
vanshu778 <vthesiya778@gmail.com>
2024-09-29 16:41:25 +0530

    add data

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,38 @@ app.get('/',(req,res)=>{
     res.send('Server is ready');
 });
 
+//get a list of 5 jokes
+app.get('/jokes',(req,res)=>{
+    const jokes = [
+        {
+        id:1,
+        title: 'A joke',
+        content: 'This is a joke'
+    },
+    {
+        id:2,
+        title: 'Another joke',
+        content: 'This is another joke'
+    },
+    {
+        id:3,
+        title: 'A  third joke',
+        content: 'This is a third joke'
+    },
+    {
+        id:4,
+        title: 'A fourth joke',
+        content: 'This is a fourth joke'
+    },
+    {
+        id:5,
+        title: 'A fifth joke',
+        content: 'This is a fifth joke'
+    }
+];
+res.send(jokes);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port,() => {
